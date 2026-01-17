22345ee026e1f049397d902b7f3aefde9dda96db
hutor-hash <borisko0482@gmail.com>
2026-01-17 18:33:42 +0100

    angular implemented front end

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,7 +3,12 @@ import { ApiService } from './services/message';
 import { App } from './app';
 import { LoginComponent } from './components/login';
 import { HomeComponent } from './components/home';
+import { MarketplaceComponent } from "./components/marketplace";
+import { AboutComponent } from "./components/aboutus";
 
 export const routes: Routes = [ 
-{ path: '', component: HomeComponent },
-{ path: 'login', component: LoginComponent },];
+    { path: '', component: HomeComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'tickets', component: MarketplaceComponent },
+    { path: 'about', component: AboutComponent },
+    { path: '**', redirectTo: '' }];
---
+++ frontend/src/app/components/aboutus.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+    selector: 'app-about',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './aboutus.html',
+    styleUrls: ['./aboutus.css']
+})
+export class AboutComponent { }
