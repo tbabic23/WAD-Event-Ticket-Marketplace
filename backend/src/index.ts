e2a55c69200a082b344426fc0989197ee084a915
tbabic23 <tbabic23@student.foi.hr>
2026-01-21 02:32:42 +0100

    fixed server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,13 +14,15 @@ app.use(bodyParser.json());
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../../frontend/dist/frontend')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../frontend/dist/frontend/index.html'));
-});
 app.use('/api', testRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/user', adminPanelRouter); 
 app.use('/api/events', eventsRouter);
+
+app.use(express.static(path.join(__dirname, '../../frontend/dist/frontend')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../frontend/dist/frontend/index.html'));
+});
 
 export default app;
